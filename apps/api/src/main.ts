083c463ee7999fc14eb04fbc42fef76134f0a1a2
sp3dych <pedychsergii@gmail.com>
2022-11-16 15:17:55 -0800

    Init project

---
+++ apps/api/src/main.ts
@@ -0,0 +1,24 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal api to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { urlencoded, json } from 'express';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
